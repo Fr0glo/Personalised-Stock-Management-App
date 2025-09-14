1e7257942c9bf88fdb2f0b85d420a0e0b14ff279
Fr0glo <so7734r@gre.ac.uk>
2025-09-14 21:23:47 +0100

    Security Guy Page
    This is the security guy's page we will see when everything goes well we will then merge them into the main

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,19 +6,30 @@ import Stock from './pages/Stock';
 import Orders from './pages/Orders';
 import Vouchers from './pages/Vouchers';
 import Personnel from './pages/Personnel';
+import Security from './pages/Security';
 
 function App() {
   return (
     <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/stock" element={<Stock />} />
-          <Route path="/orders" element={<Orders />} />
-          <Route path="/vouchers" element={<Vouchers />} />
-          <Route path="/personnel" element={<Personnel />} />
-        </Routes>
-      </Layout>
+      <Routes>
+        {/* Security page - no layout (full screen) */}
+        <Route path="/security" element={<Security />} />
+        
+        
+        
+        {/* Main app pages - with layout */}
+        <Route path="/*" element={
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/stock" element={<Stock />} />
+              <Route path="/orders" element={<Orders />} />
+              <Route path="/vouchers" element={<Vouchers />} />
+              <Route path="/personnel" element={<Personnel />} />
+            </Routes>
+          </Layout>
+        } />
+      </Routes>
     </Router>
   );
 }
